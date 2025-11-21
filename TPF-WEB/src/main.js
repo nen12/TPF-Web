e45770b2a9694e5b58d936841fbe4626f3f8a6c0
edgar <2252988@bdeb.qc.ca>
2025-11-21 16:05:49 -0500

    Animation PageTitre (effet flottant + effet de scroll)

--- TPF-WEB/src/main.js
+++ TPF-WEB/src/main.js
@@ -4,9 +4,16 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import gsap from 'gsap'
 
+//Importation du plugin scrolltrigger.
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+ 
+//Importation du plugin scrollTrigger pour l'animation de texte de Hero
+gsap.registerPlugin(ScrollTrigger);
+
+
 
 gsap.to(".titre", {
-   scale: 1.02,
+   scale: 1.03,
    duration: 1,
    ease: "ease.inOut",
    repeat: -1,
@@ -14,7 +21,23 @@ gsap.to(".titre", {
     
 });
 
+gsap.to(".titre", {
+    scrollTrigger: {
+        trigger: '.titre',
+        start: "170% 250px", 
+        end: "400% 200px",
+        scrub: 1, 
+ 
+        toggleActions: "play none none reverse" 
+    },
+ 
+    
+    y: 200,
+    delay:0.5,
+    ease: 'expo',
+    duration: 2,
+    opacity: 0,
+});   
 
-   
 
 
