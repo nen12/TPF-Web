3ab6e790adb3dcccf9a57ba192b6aecf61b8d939
edgar <2252988@bdeb.qc.ca>
2025-11-21 15:57:03 -0500

    Ajout du calendrier et Animation temporaire du texte Titre, +Hover and Active

--- TPF-WEB/src/main.js
+++ TPF-WEB/src/main.js
@@ -2,4 +2,19 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import gsap from 'gsap'
+
+
+gsap.to(".titre", {
+   scale: 1.02,
+   duration: 1,
+   ease: "ease.inOut",
+   repeat: -1,
+   yoyo: true
+    
+});
+
+
+   
+
 
