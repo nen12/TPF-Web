34585e11322b5f44546babad742f44fd3a6c225c
RendyC <dieudonnchrysler@gmail.com>
2025-12-04 12:39:18 -0500

    Correction des éléments de la page d'accueil et ajout de plusieurs commentaires dans le css

--- TPF-WEB/src/main.js
+++ TPF-WEB/src/main.js
@@ -4,16 +4,22 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import gsap from 'gsap'
 
+//Importation du plugin scrolltrigger.
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 // Sélection et interaction avec le bouton menu burger
 const menuBurger = document.querySelector (".menu-burger")
 const navLinks = document.querySelector (".nav-links")
     menuBurger.addEventListener('click',()=>
     navLinks.classList.toggle('mobile-menu'));
+  
+    // Fonction pour l'interaction avec le bouton de l'hero.
+    const button = document.getElementById ('bouton-nöel'); //Sélectionne le bouton par son ID.
 
-    
-//Importation du plugin scrolltrigger.
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
+    button.addEventListener ('click', function() { // Ajout d'un écouteur d'événement.
+    window.location.href = 'Page2.html'; //Redirige vers une autre page du site.
+});
+ 
 //Importation du plugin scrollTrigger pour l'animation de texte de Hero
 gsap.registerPlugin(ScrollTrigger);
 
