fac8b45df020cb0f0c63b36b167e5c67091cce70
nen12 <6273595@bdeb.qc.ca>
2025-12-12 16:08:39 -0500

    fix mise en ligne

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: '/TPF-WEB/',
+    base: '/TPF-Web/',
     build: {
         emptyOutDir: true,
             rollupOptions: {
